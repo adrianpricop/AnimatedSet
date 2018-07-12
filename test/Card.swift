dea0854011f3ff233b303f923029ced1dce3a3b6
apple <apple@MacBook-Air-apple.local>
2018-07-12 11:55:01 +0300

    animated set

---
+++ test/Card.swift
@@ -0,0 +1,79 @@
+//
+//  Card.swift
+//  test
+//
+//  Created by apple on 05/06/2018.
+//  Copyright © 2018 wolfpack. All rights reserved.
+//
+
+import Foundation
+
+
+struct Card: Hashable {
+    
+
+    var matching = false
+    var isSelected = false
+    var identifier: Int
+    
+    let cardColor: CardColor 
+    let cardSimbol: CardSimbol
+    let cardNumber: CardNumber
+    let cardShading: CardShading
+    
+    var hashValue: Int
+    {
+        return identifier.hashValue
+    }
+    
+    static func == (lhs: Card ,rhs: Card) ->Bool
+    {
+        return lhs.identifier == rhs.identifier
+    }
+    
+//    static var identifierFactori = 0
+//
+//    static func getUnoqueIdentifier() -> Int
+//    {
+//        identifierFactori += 1
+//        return identifierFactori
+//    }
+//
+//    init()
+//    {
+//        self.identifier = Card.getUnoqueIdentifier()
+//    }
+    
+    enum CardSimbol
+    {
+        case diamond
+        case oval
+        case squiggle
+
+        static let allValues = [diamond, oval, squiggle]
+    }
+    enum CardColor
+    {
+        case red
+        case green
+        case purple
+
+        static let allValues = [red, green, purple]
+    }
+    enum CardNumber
+    {
+        case one
+        case two
+        case three
+
+        static let allValues = [one, two, three]
+    }
+    enum CardShading
+    {
+        case full
+        case empty
+        case stripped
+
+        static let allValues = [full, empty, stripped]
+    }
+}
---
+++ test/ViewController.swift
@@ -0,0 +1,178 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by apple on 05/06/2018.
+//  Copyright © 2018 wolfpack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, DidFinishAnimating
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        game.cardGenerator()
+        drawThreeCardsButton.noSelect()
+        showSetButton.noSelect()
+        newGameButton.noSelect()
+        scoreBord.layer.masksToBounds = true
+        scoreBord.layer.cornerRadius = 10
+        cardsContainerView.inPlayCards = game.inPlayCards
+        updateViewFromModel()
+        cardsContainerView.delegate = self
+        drawThreeCardsButton.isEnabled = false
+        showSetButton.isEnabled = false
+        newGameButton.isEnabled = false
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        cardsContainerView.buttonDeckFrame = CGRect(x: drawThreeCardsButton.frame.origin.x ,
+                                                y: drawThreeCardsButton.frame.origin.y - drawThreeCardsButton.frame.height - 8 ,
+                                                width: drawThreeCardsButton.frame.width,
+                                                height: drawThreeCardsButton.frame.height)
+        cardsContainerView.buttonDiscardPile = showSetButton
+        cardsContainerView.inPlayCards = game.inPlayCards
+        cardsContainerView.numberOfButtons += game.inPlayCards.count
+        
+        updateViewFromModel()
+
+        
+        
+    }
+    
+        
+    @IBOutlet var draw3Cards: UISwipeGestureRecognizer!
+    
+    @IBAction func draw3Cards(_ sender: UISwipeGestureRecognizer) {
+        if game.deck.count > 2
+        {
+            game.drawThreeCards()
+            cardsContainerView.inPlayCards = game.inPlayCards
+            cardsContainerView.numberOfButtons += 3
+            updateViewFromModel()
+            
+        }
+        if game.deck.count < 3
+        {
+            drawThreeCardsButton.isHidden = true
+        }
+        drawThreeCardsButton.isEnabled = false
+        showSetButton.isEnabled = false
+        newGameButton.isEnabled = false
+    }
+    @IBOutlet var shuffleCards: UIRotationGestureRecognizer!
+    @IBAction func shuffleCards(_ sender: Any) {
+//        game.shuffleCards()
+//        updateViewFromModel()
+//        cardsContainerView.inPlayCards = game.inPlayCards
+        
+    }
+    @IBOutlet weak var cardsContainerView: ContainerView!
+    
+    
+    
+    @IBOutlet weak var drawThreeCardsButton: UIButton!
+    
+    @IBOutlet weak var showSetButton: UIButton!
+    
+    @IBOutlet weak var newGameButton: UIButton!
+    
+    @IBOutlet weak var scoreBord: UILabel!
+    
+    var game = SetGame()
+    
+    func FinishedAnimating()
+    {
+        addTaregetFunc()
+        drawThreeCardsButton.isEnabled = true
+        showSetButton.isEnabled = true
+        newGameButton.isEnabled = true
+    }
+    
+    
+    @IBAction func newGameButton(_ sender: UIButton)
+    {
+        game.newGame()
+        cardsContainerView.inPlayCards.removeAll()
+        cardsContainerView.removeAllButtons()
+        
+        cardsContainerView.inPlayCards = game.inPlayCards
+        cardsContainerView.numberOfButtons = game.inPlayCards.count
+
+        drawThreeCardsButton.isHidden = false
+
+        
+    }
+    
+    @IBAction func showSet(_ sender: UIButton)
+    {
+        if game.findSet()
+        {
+            updateViewFromModel()
+            cardsContainerView.inPlayCards = game.inPlayCards
+        }
+        
+    }
+    
+    @IBAction func drawThreeCards(_ sender: UIButton)
+    {
+        if game.deck.count > 2
+        {
+            game.drawThreeCards()
+            cardsContainerView.inPlayCards = game.inPlayCards
+            cardsContainerView.numberOfButtons += 3
+            updateViewFromModel()
+
+        }
+        if game.deck.count < 3
+        {
+            drawThreeCardsButton.isHidden = true
+        }
+        drawThreeCardsButton.isEnabled = false
+        showSetButton.isEnabled = false
+        newGameButton.isEnabled = false
+    }
+
+    func addTaregetFunc()
+    {
+        for button in cardsContainerView.buttons
+        {
+            button.addTarget(self, action: #selector(didTapCard), for: .touchUpInside)
+        }
+    }
+
+    @objc func didTapCard(sender: UIButton)
+    {
+        
+        let index = cardsContainerView.buttons.index(of: sender as! DrawButton)!
+        game.selectCard(at: index)
+        if game.selectedCards.count == 3
+        {
+            cardsContainerView.inPlayCards = game.inPlayCards
+            if game.isSet(for: game.selectedCards)
+            {
+                cardsContainerView.selectedCards = game.selectedCards
+                game.verifySelectedCards()
+                cardsContainerView.inPlayCards = game.inPlayCards
+            }
+            
+        }else
+        {
+            cardsContainerView.inPlayCards = game.inPlayCards
+        }
+        updateViewFromModel()
+        
+    }
+    func updateViewFromModel()
+    {
+        let score = game.score
+        scoreBord.text = "score: \(score)"
+        
+
+    }
+    
+    
+}
+
+
